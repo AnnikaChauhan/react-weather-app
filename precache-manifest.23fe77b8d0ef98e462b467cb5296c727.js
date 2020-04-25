9bc885ac79487437d4aa0f14e4672b5584603693
Annika Chauhan <annikac0711@gmail.com>
2020-04-25 17:12:23 +0100

    Updates

---
+++ precache-manifest.23fe77b8d0ef98e462b467cb5296c727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c575f3b2a0e07ef18294491dccb0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c80748f0679a458bf619",
+    "url": "/static/css/main.53ba0f05.chunk.css"
+  },
+  {
+    "revision": "e14975c8826417e7b752",
+    "url": "/static/js/2.06602091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06602091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80748f0679a458bf619",
+    "url": "/static/js/main.23d74546.chunk.js"
+  },
+  {
+    "revision": "c2287fd55f8b7ecdc110",
+    "url": "/static/js/runtime-main.54ac6210.js"
+  }
+]);
